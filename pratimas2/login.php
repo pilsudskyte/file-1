8ecb9456771acb97ee7cb29f5d47be663f22f5c4
pilsudskyte <kristina.pilsudskyet@gmail.com>
2018-05-08 13:41:12 +0300

    6

--- pratimas2/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_POST['user'] == 'Jonas' && $_POST['pass'] =='123') {
-    //echo "YES";
-    include "info.php";
-    else {
-        //echo "No";
-        include "index.php";
-
-    }
